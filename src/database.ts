bbaa010866afb9b86cc67fb7f36515aa65be3e63
Lucas Marques <lucasses10@gmail.com>
2024-07-04 10:58:50 -0300

    feat: add route transactions

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,15 @@
 import { knex as setupKnex, Knex } from 'knex'
+import { env } from './env'
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db',
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
-    directory: './db/migraions',
+    directory: './db/migrations',
   },
 }
 
